dd2cb858774ad5d89a79b42c7c87bbe6b7f833cf
ThihaThant <thihathant.22@ichat.sp.edu.sg>
2023-04-25 14:09:40 +0800

    commit2

--- buy-food/App.js
+++ buy-food/App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 //Test lah lah lah lah heheeho
+//commit 2
 // You can import from local files
 import AssetExample from './components/AssetExample';
 
