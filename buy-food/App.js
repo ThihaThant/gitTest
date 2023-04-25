0474a1819cee5179b8ab5b43c106b27f106d1939
ThihaThant <thihathant.22@ichat.sp.edu.sg>
2023-04-25 14:10:01 +0800

    commit 3

--- buy-food/App.js
+++ buy-food/App.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 //Test lah lah lah lah heheeho
 //commit 2
+//commit 3
 // You can import from local files
 import AssetExample from './components/AssetExample';
 
